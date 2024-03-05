e65f28b815c8ce86c1a7f7fbd348e6d38597a22c
Marie-Jeanne Cormier <mariejeanne01@hotmail.ca>
2024-03-05 10:55:31 -0500

    gdm login rm

--- configuration.nix
+++ configuration.nix
@@ -123,7 +123,7 @@
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "marielabro";
-  services.xserver.displayManager.gdm.wayland = true;
+  #services.xserver.displayManager.gdm.wayland = true;
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   #systemd.services."getty@tty1".enable = false;
